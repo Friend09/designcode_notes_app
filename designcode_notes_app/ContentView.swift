d0378fd71f10e1ed9e7f936684520dd51e1a67f6
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2025-04-04 13:04:09 -0400

    Initial Commit

---
+++ designcode_notes_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  designcode_notes_app
+//
+//  Created by raghuvamsi ayapilla on 4/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ designcode_notes_app/designcode_notes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  designcode_notes_appApp.swift
+//  designcode_notes_app
+//
+//  Created by raghuvamsi ayapilla on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct designcode_notes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
