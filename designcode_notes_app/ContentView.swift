286106054924d49ff6d55d60c5c700635ff78a33
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2025-04-27 14:12:19 -0400

    Enhance button styling in ContentView with a gradient background, improved overlay, and shadow effects for better visual appeal.

--- designcode_notes_app/ContentView.swift
+++ designcode_notes_app/ContentView.swift
@@ -10,7 +10,7 @@ struct ContentView: View {
                 .font(.title)
                 .fontWeight(.bold)
             Text("Transform your thoughts into well-structured notes using artificial intelligence.")
-            
+
 
             TextEditor(text: $inputText)
                 .frame(height: 200)
@@ -31,9 +31,33 @@ struct ContentView: View {
                 }
                 .padding()
                 .frame(maxWidth: .infinity)
-                .background(Color.blue)
-                .foregroundColor(.white)
+                
+                .background(MeshGradient(width: 3, height: 3, points: [
+                    .init(0, 0), .init(0.5, 0), .init(1, 0),
+                    .init(0, 0.5), .init(0.5, 0.5), .init(1, 0.5),
+                    .init(0, 1), .init(0.5, 1), .init(1, 1)
+                ], colors: [
+                    .red, .purple, .indigo,
+                    .orange, .white, .blue,
+                    .yellow, .green, .mint
+                ]).cornerRadius(16))
+                .foregroundColor(.black)
                 .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(.white, lineWidth: 3)
+                        .blur(radius: 2)
+                        .blendMode(.overlay)
+                )
+                .background(.black)
+                .cornerRadius(16)
+                .background(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(.black.opacity(0.5), lineWidth: 1)
+                )
+                .shadow(color: .black.opacity(0.15), radius: 20, x: 0, y: 20)
+                .shadow(color: .black.opacity(0.1), radius: 15, x: 0, y: 15)
+                
             }
             .disabled(isLoading || inputText.isEmpty)
             .padding(.horizontal)
